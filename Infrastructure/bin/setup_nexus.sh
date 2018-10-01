7b03e06fd03de13a8fe2a44820b149116a8397e3
Machiel <machiel.van.ampting@trivento.nl>
2018-10-01 20:45:50 +0200

    all objects in template, wait for nexus with health check

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -9,45 +9,30 @@ fi
 GUID=$1
 echo "Setting up Nexus in project $GUID-nexus"
 
-# Code to set up the Nexus. It will need to
-# * Create Nexus
-# * Set the right options for the Nexus Deployment Config
-# * Load Nexus with the right repos
-# * Configure Nexus as a docker registry
-# Hint: Make sure to wait until Nexus if fully up and running
-#       before configuring nexus with repositories.
-#       You could use the following code:
-# while : ; do
-#   echo "Checking if Nexus is Ready..."
-#   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
-#   [[ "$?" == "1" ]] || break
-#   echo "...no. Sleeping 10 seconds."
-#   sleep 10
-# done
-
-# Ideally just calls a template
-# oc new-app -f ../templates/nexus.yaml --param .....
-
-# To be Implemented by Student
-
-
 #
 # Create objects
 #
+oc project $GUID-nexus
 oc process -f Infrastructure/templates/nexus.yaml -p GUID=${GUID} | oc create -f -
 
 #
 # wait for nexus
 #
-while : ; do
-    oc get pod -n ${GUID}-nexus | grep '\-1\-' | grep -v deploy | grep "1/1"
-    if [ $? == "1" ] 
-      then 
-        sleep 10
-      else 
-        break 
-    fi
+set -x
+NEXUS_URL=$(oc get route nexus3 --template='{{ .spec.host }}' -n ${GUID}-nexus)/service/metrics/ping
+until  curl  -u admin:admin123 ${NEXUS_URL}; do
+  echo "."
+  sleep 10
 done
+#while : ; do
+#    oc get pod -n ${GUID}-nexus | grep '\-1\-' | grep -v deploy | grep "1/1"
+#    if [ $? == "1" ] 
+#      then 
+#        sleep 10
+#      else 
+#        break 
+#    fi
+#done
 
 #
 # Configure Nexus
